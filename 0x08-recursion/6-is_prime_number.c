02b81617f49883d230c2c8c9e60c6610a8302fd2
ZAKARIAJMD <zakaria.jammoud2002@gmail.com>
2023-08-30 11:08:23 -0700

    sqrt

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * is_prime_number - prints a string in reverse
+ * @n: int to calculate its factoriel
+ * 
+ * Return: puissance
+ *
+ */
+int is_prime_number(int n)
+{
+	if (y > 0)
+	{
+		return (x * _pow_recursion(x, y - 1));
+	}
+	if (y == 0)
+	{
+		return (1);
+	}
+	return (-1);
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -7,3 +7,5 @@ int _putchar(char c);
 int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int _sqrt_two(int n, int c);
