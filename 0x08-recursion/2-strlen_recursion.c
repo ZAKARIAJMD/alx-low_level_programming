b43f70cb821e60e97afe1431a506e6900705c9d3
ZAKARIAJMD <zakaria.jammoud2002@gmail.com>
2023-08-30 07:53:34 -0700

    lenght

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - prints a string in reverse
+ * @s: string to print
+ * Return: nothing
+ *
+ */
+int _strlen_recursion(char *s)
+{
+	int i = 0 ;
+
+	if (*s != '\0')
+	{
+		i++;
+		return (i + _strlen_recursion(s + 1);
+	}
+}
