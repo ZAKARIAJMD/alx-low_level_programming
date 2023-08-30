7d9796ec00c9390579d0e83b49605fe4b4dc9129
ZAKARIAJMD <zakaria.jammoud2002@gmail.com>
2023-08-30 08:30:35 -0700

    lenght

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - prints a string in reverse
+ * @x: int to calculate its factoriel
+ * @y: int
+ * Return: puissance
+ *
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y > 0)
+	{
+		return (x * _pow_recursion(x,y - 1));
+	}
+	if (y == 0)
+	{
+		return (1);
+	}
+	return (-1);
+}
