8a44254d831aa1180893eff8ef4e1eca3c6fbd70
ZAKARIAJMD <zakaria.jammoud2002@gmail.com>
2023-08-30 06:19:42 -0700

    add rev

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,16 +1,16 @@
 include "main.h"
 
 /**
- * _puts_recursion - prints a string, followed by a new line
+ * _print_rev_recursion - prints a string in reverse
  * @s: string to print
  * Return: nothing
  *
  */
 void _print_rev_recursion(char *s);
 {
-	if(*s != '/0')
+	if (*s != '/0')
 	{
-		_print_rev_recursion(s+1);
+		_print_rev_recursion(s + 1);
 		_putchar(*s);
 	}
 }
