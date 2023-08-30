5a8ad87bebe9cf50c6086c700f64d129a8052c5d
ZAKARIAJMD <zakaria.jammoud2002@gmail.com>
2023-08-30 08:12:51 -0700

    lenght

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * factorial - prints a string in reverse
+ * @n: int to calculate its factoriel
+ * Return: lenght
+ *
+ */
+int factorial(int n)
+{
+	
+	if (n > 0)
+	{
+		return (n * factorial(n-1) );
+	}else if(n == 0)
+	{
+		return (1);
+	}
+
+	return (-1);
+}
