ef3e3955b38e56a6b7584afb3aee22d1c58ac0f0
ZAKARIAJMD <zakaria.jammoud2002@gmail.com>
2023-08-30 08:14:15 -0700

    lenght

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -7,15 +7,14 @@
  *
  */
 int factorial(int n)
-{
-	
+{	
 	if (n > 0)
 	{
-		return (n * factorial(n-1) );
+		return (n * factorial(n-1));
 	}else if(n == 0)
 	{
 		return (1);
-	}
+	}²
 
 	return (-1);
 }
