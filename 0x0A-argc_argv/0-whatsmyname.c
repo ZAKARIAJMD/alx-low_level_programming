882bb2593b7a7370d0dbbae25c1c9ea62eb225bf
ZAKARIAJMD <zakaria.jammoud2002@gmail.com>
2023-09-01 14:34:39 -0700

    argv project

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,14 +1,16 @@
-#include<stdio.h>
+#include <stdio.h>
+
 /*
- * main : main function
- * RETURN: 0 
+ * main - main function
+ * RETURN: Always 0 (Success)
  *
  */
 
-int main(int argc,char *argv[])
+int main(int argc, char *argv[])
 {
 	int x = argc;
+
 	x++;
-	printf("%s\n",argv);
+	printf("%s\n", argv[0]);
 	return (0);
 }
