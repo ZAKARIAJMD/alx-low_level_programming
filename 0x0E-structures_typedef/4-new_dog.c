53a9e5bd013a771fcd68f646f4a304639e3b20d7
ZAKARIAJMD <zakaria.jammoud2002@gmail.com>
2023-09-12 00:39:39 +0100

    struct 3

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -14,13 +14,13 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *d;
 
-	d = malloc(sizeof(dog_t));
 	if (d == NULL)
 		return (NULL);
 	if (name == NULL || owner == NULL)
 		return (NULL);
 	d->name = malloc(sizeof(name));
 	d->owner = malloc(sizeof(owner));
+	d = malloc(sizeof(dog_t));
 	d->name = name;
 	(*d).age = age;
 	d->owner = owner;
