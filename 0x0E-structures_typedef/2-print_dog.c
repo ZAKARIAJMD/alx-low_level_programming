8d3448c70734d4b142d7da95842e2e7ba65040f0
ZAKARIAJMD <zakaria.jammoud2002@gmail.com>
2023-09-11 19:35:27 +0100

    struct 2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,8 +9,7 @@
  */
 void print_dog(struct dog *d)
 {
-	printf("Name:%s\n", (d)->name);
-	printf("Age:%f\n", (*d).age);
-	printf("Owner:%s\n", (d)->owner);
-
+	printf("Name: %s\n", (d)->name);
+	printf("Age: %f\n", (*d).age);
+	printf("Owner: %s\n", (d)->owner);
 }
