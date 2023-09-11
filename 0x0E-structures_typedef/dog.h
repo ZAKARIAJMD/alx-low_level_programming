8a79b312cef508e7a159964adac3ca2ac8c05145
ZAKARIAJMD <zakaria.jammoud2002@gmail.com>
2023-09-11 18:45:08 +0100

    struct 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,8 +1,15 @@
-#ifndef he_H__
-#define he_H__
-typedef struct dog{
+#ifndef RRHE_H__
+#define RRHE_H__
+/**
+ * struct dog - structure
+ *@name: name
+ *@age: age
+ *@owner: owner
+ */
+typedef struct dog
+{
 	char *name;
 	float age;
 	char *owner;
-}dog;
+} dog;
 #endif
