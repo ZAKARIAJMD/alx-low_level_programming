7c0216305cbaa29264a31d6dd345ee77a59c31b5
ZAKARIAJMD <zakaria.jammoud2002@gmail.com>
2023-09-12 01:19:23 +0100

    free dogs

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -9,8 +9,11 @@
  */
 void free_dog(dog_t *d)
 {
-	free(d->name);
-	free(d->age);
-	free(d->owner);
+	if (d == 0)
+		return;
+	if (d->name != 0)
+		free(d->name);
+	if (d->name != 0)
+		free(d->owner);
 	free(d);
 }
