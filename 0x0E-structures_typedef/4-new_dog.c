cd0a3accf8b8c8a514a8758e41d600dcddf98463
ZAKARIAJMD <zakaria.jammoud2002@gmail.com>
2023-09-12 01:00:11 +0100

    struct 3

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -26,11 +26,14 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (d->name == 0)
 	{
 		free(d->name);
+		free(d);
 		return (NULL);
 	}
 	if (d->owner == 0)
 	{
 		free(d->owner);
+		free(d->name);
+		free(d);
 		return (NULL);
 	}
 	d->name = name;
