ace040d3f86ef9b9cff62615a4a2b5ac614c25df
ZAKARIA JAMMOUD <108638341+ZAKARIAJMD@users.noreply.github.com>
2024-01-10 22:41:04 +0100

    Add files via upload

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * is_prime_number - prints a string in reverse
+ * @n: int to calculate its factoriel
+ * Return: puissance
+ *
+ */
+int is_prime_number(int n)
+{
+	if (n <= 1)
+	{
+		return (0);
+	}
+	return (_pr(n, 2));
+}
+
+/**
+ * _pr - try
+ *
+ * @n: number
+ * @c: number
+ * Return: prime
+ *
+ */
+
+int _pr(int n, int c)
+{
+	if (c >= n)
+	{
+		return (1);
+	}
+	if ((n % c) == 0)
+	{
+		return (0);
+	}
+	return (_pr(n, c + 1));
+}
