b5fd6339e2fe56e2a8414da9a94d24c293809c68
ZAKARIA JAMMOUD <108638341+ZAKARIAJMD@users.noreply.github.com>
2024-01-17 21:02:06 +0100

    Add files via upload

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - Entry point
+ *@argc: input
+ *@argv: pointer
+ * Return: Always 0 (Success)
+ *
+ */
+
+int main(int argc, char *argv[])
+{
+	char x[100];
+
+	strcpy(x, *argv);
+	printf("%d\n", argc - 1);
+	return (0);
+}
